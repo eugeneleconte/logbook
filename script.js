5dd147e9e3df73b32a347597f0fa4aefbd2ddf5c
Eugene Leconte <eugeneleconte@outlook.com>
2025-01-31 12:50:31 +0100

    logbook

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//SIDENAV
+
 var sidenav = document.getElementById("mySidenav");
 var openBtn = document.getElementById("openBtn");
 var closeBtn = document.getElementById("closeBtn");
@@ -14,3 +16,37 @@ function openNav() {
 function closeNav() {
   sidenav.classList.remove("active");
 }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerImg = document.querySelector('.burger-icon img');
+
+  if (burgerImg) {
+    const updateImageOnScroll = () => {
+      const viewportWidth = window.innerWidth;
+      const scrollY = window.scrollY;
+
+      // Récupérer l'URL actuelle de l'image
+      const currentSrc = burgerImg.src;
+
+      // Extraire la partie après 'image/'
+      const baseUrl = currentSrc.split('image/')[0] + 'image/';
+      let newImage = '';
+
+      if (viewportWidth <= 800 && scrollY >= 216) {
+        newImage = 'emoji_burger_flashred.png';
+      } else {
+        newImage = 'emoji_burger.png';
+      }
+
+      // Mettre à jour uniquement la partie de l'image
+      if (currentSrc.split('image/')[1] !== newImage) {
+        burgerImg.src = baseUrl + newImage;
+      }
+    };
+
+    // Écoute les événements de scroll et de redimensionnement
+    window.addEventListener('scroll', updateImageOnScroll);
+    window.addEventListener('resize', updateImageOnScroll);
+  }
+});
